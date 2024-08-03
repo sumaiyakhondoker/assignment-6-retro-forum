672b81487f8008789616fd38a6a7379ed9163c10
sumaiyakhondoker <sumaiyakhondoker85@gmail.com>
2024-08-03 11:07:20 +0600

    header added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,63 @@
-console.log('connected');
+tippy("[data-tippy-content]");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const loadData = async()=>{
+//     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+//     const data = await res.json();
+//     const phones = data.data;
+//     // console.log(phones);
+//     displayPhones(phones)
+// }
+
+
+// const displayPhones = (phones)=>{
+// // console.log(phones);
+// const phoneContainer = document.getElementById('phone-container')
+// phones.forEach(phone =>{
+// console.log(phone);
+// const phoneCard = document.createElement('div')
+// phoneCard.classList = `card bg-base-100 w-96 shadow-xl`;
+// phoneCard.innerHTML = `
+//     <figure class="px-10 pt-10">
+//     <img
+//     src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+//     alt="Shoes"
+//     class="rounded-xl" />
+//     </figure>
+//     <div class="card-body items-center text-center">
+//     <h2 class="card-title">Shoes!</h2>
+//     <p>If a dog chews shoes whose shoes does he choose?</p>
+//     <div class="card-actions">
+//     <button class="btn btn-primary">Buy Now</button>
+//     </div>
+//     </div>
+// `;
+// phoneContainer.appendChild(phoneCard);
+
+
+
+// });
+// }
+// loadData()                                                                                                                                                     
